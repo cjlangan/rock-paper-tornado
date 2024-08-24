79b07b61db225c2dd8aa245cc0faf96f6f0057a2
cjlangan <connorjameslangan@gmail.com>
2024-08-24 00:29:36 -0500

    Deciding how to interact with C files

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Select the input field and button
+const { exec } = require('child_process');
 const userInput = document.getElementById('userInput');
 const submitButton = document.getElementById('submitButton');
 
@@ -9,5 +10,8 @@ submitButton.addEventListener('click', () => {
     
     // Print the input value to the console
     console.log('User input:', inputValue);
+
+    // Execute a C program
+    // exec('./hello');
 });
 
